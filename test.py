568436378b2a61a514048c9fecdb4fc9f393788c
Marcus Arzadon <Marcus.Arzadon@users.noreply.github.com>
2020-12-13 13:04:21 -0600

    finalized project

--- test.py
+++ test.py
@@ -1,29 +1,26 @@
-
+'''
 import networkx as nx
-import matplotlib as plt
 from functions import *
-graph_data = open('G1.txt','r')
+from algorithms import prims_algorithm
+from drawing import *
+'''
+import networkx as nx
+from functions import *
+from algorithms import prims_algorithm
+from drawing import *
 
-G = nx.read_weighted_edgelist(graph_data, nodetype = int)
 
-T = prims_initialize(G, 4)
-T.add_edge(2,4)
-T.add_edge(1,3)
-T.add_edge(2,3)
-T.add_edge(4,5)
-print(f'The vertex set of G is V(G) = {V(G)}')
-print()
-print(f'The edge set of G is E(G) = {E(G)}')
-print()
-print(f'is T spanning? {is_spanning(G,T)}')
-
-def possible_edges(G,T):
-    return [e for e in list(G.edges(V(T))) if e[0] not in V(T) or e[1] not in V(T)]
-
-def min_prims_edge(G,T):
-    possible = possible_edges(G, T)
-    minimum = max
-    for x in range(0,len(possible),1):
-        if cost(G,possible[x]) < minimum:
-            minimum = cost(G,possible[x])
-    return minimum
+graph_data = open('test-graphs/G1.txt', 'r')
+'''
+will open and return a file object from test-graphs folder in this case a graph to be read,
+if file is not found will raise an error
+'''
+G=nx.read_weighted_edgelist(graph_data, nodetype=int)
+'''
+NetworkX will then generate a plot for the chosen file
+and will ensure returning a hashable type
+'''
+T= prims_algorithm(G, 0, show_graph = True, show_cost = True)
+'''
+Will then run with 0 as the starting vertex
+'''
